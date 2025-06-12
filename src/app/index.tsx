cc6cd0618df0d0bb66249842b57e7788a9e043a6
Gabriel Dias <diasgabriel246@gmail.com>
2025-06-12 15:15:07 -0300

    fix: correct app flow

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       if (token) {
         router.replace("/main");
       } else {
-        router.replace("/main");
+        router.replace("/login");
       }
     };
 
