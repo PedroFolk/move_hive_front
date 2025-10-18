589838dc10c5cdd93c735d7cdd8a4545ec10a0f3
pedroFolk <pedro.o.folkmann@gmail.com>
2025-10-18 16:33:03 -0300

    Fix: Main

--- src/app/main/index.tsx
+++ src/app/main/index.tsx
@@ -40,16 +40,7 @@ export default function Main() {
 
 
   useEffect(() => {
-    console.log("Firebase config:", {
-      FIREBASE_API_KEY: Constants.expoConfig?.extra?.FIREBASE_API_KEY,
-      FIREBASE_AUTH_DOMAIN: Constants.expoConfig?.extra?.FIREBASE_AUTH_DOMAIN,
-      FIREBASE_DATABASE_URL: Constants.expoConfig?.extra?.FIREBASE_DATABASE_URL,
-      FIREBASE_PROJECT_ID: Constants.expoConfig?.extra?.FIREBASE_PROJECT_ID,
-      FIREBASE_STORAGE_BUCKET: Constants.expoConfig?.extra?.FIREBASE_STORAGE_BUCKET,
-      FIREBASE_MESSAGING_SENDER_ID: Constants.expoConfig?.extra?.FIREBASE_MESSAGING_SENDER_ID,
-      FIREBASE_APP_ID: Constants.expoConfig?.extra?.FIREBASE_APP_ID,
-      FIREBASE_MEASUREMENT_ID: Constants.expoConfig?.extra?.FIREBASE_MEASUREMENT_ID,
-    });
+  
 
     const checkAuth = async () => {
       
